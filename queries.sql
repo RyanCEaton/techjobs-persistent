3d449874562208b7ae7b684cf4e28849fb92bba1
RyanCEaton <ryan_eaton@comcast.com>
2021-07-29 14:39:22 -0600

    Completed Part 2

--- queries.sql
+++ queries.sql
@@ -5,7 +5,7 @@
         TABLE_SCHEMA = Database()
     AND TABLE_NAME = 'techjobs' ;
 ## Part 2: Test it with SQL
-
+    SELECT * FROM employer WHERE location = 'St. Louis City';
 ## Part 3: Test it with SQL
 
 ## Part 4: Test it with SQL
